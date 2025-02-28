d320e8a664793845138713b33da59a66db95206a
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2025-02-28 03:58:01 +0000

    learned to BuiltInNat

--- pv/BuiltInNat.v
+++ pv/BuiltInNat.v
@@ -251,7 +251,14 @@ Qed.
 Theorem iter_add:
   forall {A: Type} (n m: nat) (f: A -> A) (x: A),
     Nat.iter (n + m) f x = Nat.iter n f (Nat.iter m f x).
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+(* Admitted. 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  intros.
+  induction n; simpl.
+  + reflexivity.
+  + rewrite IHn.
+    reflexivity.
+Qed.
 
 (************)
 (** 习题：  *)
@@ -262,5 +269,11 @@ Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结
 Theorem iter_mul:
   forall {A: Type} (n m: nat) (f: A -> A) (x: A),
     Nat.iter (n * m) f x = Nat.iter n (Nat.iter m f) x.
-Admitted. (* 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
-
+(* Admitted. 请删除这一行_[Admitted]_并填入你的证明，以_[Qed]_结束。 *)
+Proof.
+  intros n; try induction n; try intros m; try induction m; simpl; try tauto.
+  intros.
+  rewrite iter_add.
+  rewrite IHm.
+  reflexivity.
+Qed.
